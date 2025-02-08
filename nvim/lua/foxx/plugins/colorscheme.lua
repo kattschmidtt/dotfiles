cbdce14f4d2d51379a06b88e6c4ebb900dafbcca
Kat <siliconfoxx@protonmail.com>
2025-02-07 21:06:22 -0500

    added things

--- nvim/lua/foxx/plugins/colorscheme.lua
+++ nvim/lua/foxx/plugins/colorscheme.lua
@@ -5,10 +5,10 @@ return {
 		config = function()
 			-- Set the color scheme to mocha
 			require("catppuccin").setup({
-				flavour = "mocha", -- specify the flavour here
+				flavour = "frappe", -- specify the flavour here
 				background = { -- optional, set background light/dark
 					light = "latte",
-					dark = "mocha",
+					dark = "frappe",
 				},
 			})
 			vim.cmd("colorscheme catppuccin")
---
+++ nvim/lua/foxx/plugins/obsidian.lua
@@ -0,0 +1,88 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   -- refer to `:h file-pattern` for more examples
+	--   "BufReadPre path/to/my-vault/*.md",
+	--   "BufNewFile path/to/my-vault/*.md",
+	-- },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			name = "Notes",
+			path = "~/Documents/Obsidian-Vault/",
+		},
+		checkbox = {
+			enabled = true,
+			position = "inline",
+			unchecked = {
+				icon = "󰄱 ",
+				highlight = "RenderMarkdownUnchecked",
+				scope_highlight = nil,
+			},
+			checked = {
+				icon = "󰱒 ",
+				highlight = "RenderMarkdownChecked",
+				scope_highlight = nil,
+			},
+			custom = {
+				todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+				important = { raw = "[~]", rendered = " ", highlight = "RenderMarkdownTodo" },
+			},
+		},
+		heading = {
+			position = "inline",
+		},
+		backgrounds = {
+			"RenderMarkdownH1Bg",
+			"RenderMarkdownH2Bg",
+			"RenderMarkdownH3Bg",
+			"RenderMarkdownH4Bg",
+			"RenderMarkdownH5Bg",
+			"RenderMarkdownH6Bg",
+		},
+		foregrounds = {
+			"RenderMarkdownH1",
+			"RenderMarkdownH2",
+			"RenderMarkdownH3",
+			"RenderMarkdownH4",
+			"RenderMarkdownH5",
+			"RenderMarkdownH6",
+		},
+
+		-- see below for full list of options 👇
+	},
+
+	{
+		-- pretty markdown
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+
+	-- markdown preview
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+}
